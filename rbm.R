1feb277e6584584b62ff7a7bbcb9e14429d92e47
weinstockj <josh82093@gmail.com>
2014-11-17 16:03:41 -0500

    updated

--- rbm.R
+++ rbm.R
@@ -3,7 +3,7 @@ source("svd_utils.R")
 df = read.csv('u_clean_normalized.data')
 USER_ID = df[, 1]
 
-mod = rbm.train(as.matrix(df[, 2:ncol(df)]), 10)
+mod = rbm.train(as.matrix(df[, 2:ncol(df)]), 200)
 
 h = rbm.up(mod, as.matrix(df[, 2:ncol(df)]))
 v = rbm.down(mod, h)
@@ -12,4 +12,4 @@ pred = cbind(USER_ID, v)
 
 pred = preProcess(as.data.frame(pred))
 
-r = evaluate(df, preProcess(as.data.frame(pred)))
+r = evaluate(df, pred)
