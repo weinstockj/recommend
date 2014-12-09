4841a951a6c64540b39035337bc745a6c94e7e09
weinstockj <josh82093@gmail.com>
2014-12-09 00:44:11 -0500

    Update svd_crossval.R

--- baseline/svd/svd_crossval.R
+++ baseline/svd/svd_crossval.R
@@ -3,26 +3,37 @@ N_FOLDS = 5
 source("svd_utils.R")
 
 rmseVector = rep(0, N_FOLDS)
-for(i in 1:N_FOLDS){
-  cat("\nfold is: ", i)
-  trainFile = sprintf("u%d.base", i)
-  testFile = sprintf("u%d.test", i)
-  trainPath = paste0(DATA_DIR, trainFile)
-  testPath = paste0(DATA_DIR, testFile)
-  trainData = cleanData(trainPath)
-  testData = cleanData(testPath)
-  svdModel = train(trainData)
-  pred = predict(testData, svdModel, 5)
-  res = evaluate(preProcess(testData), preProcess(as.data.frame(pred)))
-  rmseVector[i] = sqrt(mean(res, na.rm = T))
-}
+library("plyr")
+source("svd_utils.R")
+DATA_DIR = "~/2nd semester 2014/machine learning/project/implementation/data/ml-100k/ml-100k/"
+N_FOLDS = 5
+N_VEC = c(1, 2, 5, 10, 50, 100, 200)
 
-# old preprocessing
-# vectors = 30 -> 1.332
-# vectors = 10 -> 1.332
-# vectors = 3 -> 1.332184
-# vectors = 1 -> 1.332117
-# new preprocessing
-# all vectors -> 0.8563558
-# vectors = 30 -> 0.8560269
-# vectors = 5 -> 0.8560171
+rmseVector = list()
+index = 1
+for(nvec in N_VEC){
+  for(i in 1:N_FOLDS){
+    cat("\nfold is: ", i)
+    trainFile = sprintf("u%d.base", i)
+    testFile = sprintf("u%d.test", i)
+    trainPath = paste0(DATA_DIR, trainFile)
+    testPath = paste0(DATA_DIR, testFile)
+    trainData = cleanData(trainPath)[[2]]
+    testData = cleanData(testPath)[[2]]
+    testData = resize(trainData, testData)
+    trainDataNorm = preProcess(trainData)
+    OVERALL_MEAN = trainDataNorm[[2]]
+    COL_MEANS = trainDataNorm[[3]]
+    ROW_MEANS = trainDataNorm[[4]]
+    trainDataNorm = trainDataNorm[[1]]
+    trainDataNorm = impute(trainDataNorm)  
+    svdModel = train(trainDataNorm)
+    pred = predict(testData, svdModel, nvec)
+    pred = preProcess(as.data.frame(pred), F)
+    pred = deNormalize(pred, OVERALL_MEAN, COL_MEANS, ROW_MEANS)
+    res = evaluate(testData, pred)
+    rmseVector[[length(rmseVector) + 1]] = data.frame(rmse = sqrt(mean(res, na.rm = T)), 
+      nvec = nvec)
+    index = index + 1
+  }
+}
