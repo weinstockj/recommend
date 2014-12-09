529389754d09f9771bfe59239f80a6f5aa9a97ab
weinstockj <josh82093@gmail.com>
2014-12-09 13:12:48 -0500

    added rbm folder

--- rbm/rbm.R
+++ rbm/rbm.R
@@ -1,9 +1,10 @@
 library('deepnet')
 source("svd_utils.R")
 df = read.csv('u_clean_normalized.data')
+df = df[, -1]
 USER_ID = df[, 1]
 
-mod = rbm.train(as.matrix(df[, 2:ncol(df)]), 200)
+mod = rbm.train(as.matrix(df[, 2:ncol(df)]), hidden = 50, numepochs = 10)
 
 h = rbm.up(mod, as.matrix(df[, 2:ncol(df)]))
 v = rbm.down(mod, h)
